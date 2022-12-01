e4fd822f3fab47ddea3d276530d57a34cc5c772b
John Verrone <john.verrone@gmail.com>
2022-11-30 23:16:48 -0600

    day01

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Using [bun](https://bun.sh/) as a TypeScript runtime this year.
 To run a puzzle, ensure `bun` is installed and run `bun dayX/index.ts`
 
 ### Day 01
+
+Always an easy one to kick off of AoC. I spent majority of the time writing `console.log`s trying to understand why my solution wasn't working only to realize I wasn't initializing a value for `cals[currElf]` if it was the first row for an elf so `NaN` was happenning when trying to add values.
+
+Also accidentally submitted my answer from the test input so I had to wait to submit again with the solution from the real puzzle input.
--- day01/index.ts
+++ day01/index.ts
@@ -1,5 +1,26 @@
 import { readFileAsText } from '../utils/readFile';
 
 const contents = await readFileAsText(import.meta.dir);
+const rows = contents.split('\n');
 
-console.log(contents);
+// part 1
+const cals: number[] = [];
+let currElf = 0;
+rows.forEach((row) => {
+  if (row.length) {
+    if (!cals[currElf]) {
+      cals[currElf] = 0;
+    }
+    cals[currElf] += parseInt(row);
+  } else {
+    currElf++;
+  }
+});
+
+console.log('Part 1:', Math.max(...cals));
+
+// part 2
+const sorted = cals.sort((a, b) => b - a);
+const sum = sorted.slice(0, 3).reduce((acc, curr) => (acc += curr), 0);
+
+console.log('Part 2:', sum);
